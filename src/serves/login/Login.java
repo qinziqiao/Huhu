801f75ea4ee088d05799134b7e6b029d17a445d7
luokaijin <464807470@qq.com>
2017-09-08 17:05:27 +0800

    a
    Signed-off-by: luokaijin <464807470@qq.com>

--- src/serves/login/Login.java
+++ src/serves/login/Login.java
@@ -24,6 +24,7 @@ import com.google.gson.JsonObject;
  */
 @WebServlet("/login")
 public class Login extends HttpServlet {
+	int a=0;
 	private static final long serialVersionUID = 1L;
 	private UserTable ut; 
     /**
