9786262e4ee6540e06d31ad3bfd88b72c09692f8
qinziqiao <874982197@qq.com>
2017-09-12 16:14:18 +0800

    2017/9/12 QuestList 加photo、空数据加501错误码、register改为Register

--- src/serves/homepage/homepage.java
+++ src/serves/homepage/homepage.java
@@ -95,7 +95,7 @@ public class homepage extends HttpServlet {
 			// 对结果集进行JSON解析
 			if (rs.next() == false) {
 				// 没有找到数据
-				response.setStatus(500);
+				response.setStatus(501);
 				Response(out, false, null);
 			} else {
 				rs.beforeFirst();
