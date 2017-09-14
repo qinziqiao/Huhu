83d3da60b5f88c6418c8080be481fb100659fd52
luokaijin <464807470@qq.com>
2017-09-14 08:58:43 +0800

    Signed-off-by: luokaijin <464807470@qq.com>

--- src/serves/question/QuestList.java
+++ src/serves/question/QuestList.java
@@ -65,6 +65,8 @@ public class QuestList extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		response.setCharacterEncoding("utf-8");
 		PrintWriter out=response.getWriter();
 //		String id=IsLogin.isLogin(request);
 //		if(id==null){
